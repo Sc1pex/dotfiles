cc767c1e2a62c631b497b45ccc1eb9f29469fe47
Sc1pex <scipex.dev@gmail.com>
2024-09-16 02:24:20 +0300

    Remove config.fish symlink

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,16 @@
-/var/home/scipex/sync/config.fish
+set fish_greeting
+
+# Starship prompt
+if status --is-interactive
+   source ("starship" init fish --print-full-init | psub)
+end
+
+# Custom aliases
+alias vim="nvim"
+alias ls="eza -la"
+alias code="code --enable-features=UseOzonePlatform --ozone-platform=wayland"
+# Devcontainer aliases
+alias devcb="devcontainer build --workspace-folder ."
+alias devcu="devcontainer up --workspace-folder ."
+alias devce="devcontainer exec --workspace-folder ."
+alias dtmux="devcontainer exec --workspace-folder . tmux"
