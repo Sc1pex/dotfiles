d56bffff3c4a8dd743a1022f80a4da0b2c52c1a4
Sc1pex <scipex.dev@gmail.com>
2024-10-14 18:00:53 +0300

    nvim: neo tree

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,4 +1,14 @@
-vim.keymap.set('n', '<c-h>', '<cmd><C-U>TmuxNavigateLeft<cr>')
-vim.keymap.set('n', '<c-j>', '<cmd><C-U>TmuxNavigateDown<cr>')
-vim.keymap.set('n', '<c-k>', '<cmd><C-U>TmuxNavigateUp<cr>')
-vim.keymap.set('n', '<c-l>', '<cmd><C-U>TmuxNavigateRight<cr>')
+-- Tmux navigation
+vim.keymap.set('n', '<c-h>', '<cmd>TmuxNavigateLeft<cr>')
+vim.keymap.set('n', '<c-j>', '<cmd>TmuxNavigateDown<cr>')
+vim.keymap.set('n', '<c-k>', '<cmd>TmuxNavigateUp<cr>')
+vim.keymap.set('n', '<c-l>', '<cmd>TmuxNavigateRight<cr>')
+
+-- NeoTree
+vim.keymap.set('n', '<leader>e', '<cmd>Neotree toggle<cr>')
+
+-- General
+vim.keymap.set('n', '<leader>w', ':w<cr>')
+
+vim.keymap.set('n', '\\', '<cmd>vsplit<cr>')
+vim.keymap.set('n', '|', '<cmd>split<cr>')
