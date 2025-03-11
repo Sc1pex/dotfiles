359c631950591cc19796549920b3dbc19d0bde6f
Sc1pex <scipex.dev@gmail.com>
2025-03-11 15:45:11 +0200

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,8 +12,16 @@ alias code="code --enable-features=UseOzonePlatform --ozone-platform=wayland"
 
 set -gx EDITOR nvim
 
-function tses
-    set selected_sesh (sesh list -i -T | gum filter --limit 1 --placeholder 'Pick a sesh' --prompt='⚡')
-    sesh connect "$selected_sesh"
+# ASDF configuration code
+if test -z $ASDF_DATA_DIR
+    set _asdf_shims "$HOME/.asdf/shims"
+else
+    set _asdf_shims "$ASDF_DATA_DIR/shims"
 end
 
+# Do not use fish_add_path (added in Fish 3.2) because it
+# potentially changes the order of items in PATH
+if not contains $_asdf_shims $PATH
+    set -gx --prepend PATH $_asdf_shims
+end
+set --erase _asdf_shims
